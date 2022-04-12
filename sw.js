6092adc8c60af6f9d112f08f84c4c8096375763d
Pei-Yao Hung <peiyaoh@gmail.com>
2022-04-11 22:20:28 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ba20ab4aab0b7f194284.js"
+    "url": "webpack-runtime-2a542d93bad76b663770.js"
   },
   {
     "url": "styles.700175ca1cc75995704c.css"
@@ -45,7 +45,15 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a165f0db678b74b9dcdec218f635af6f"
+    "revision": "9f6d8e23c2cb0ddeabfbd2837749c9c1"
+  },
+  {
+    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
+    "revision": "c7047792c6f91b88e0d9abc0cd819e92"
+  },
+  {
+    "url": "page-data/app-data.json",
+    "revision": "b865c760e3fdc2ae9b336ad8ba4eabc0"
   },
   {
     "url": "polyfill-90341bc35b5255d1a92f.js"
